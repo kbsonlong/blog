c80bf7be7a38646af5852fa08acd61762d6c2637
kbsonlong <kbsonlong@gmail.com>
2018-01-30 15:46:36 +0800

    aboutme

--- article/views.py
+++ article/views.py
@@ -71,18 +71,40 @@ def blog_search(request):
 
 
 class RSSFeed(Feed) :
-    title = "RSS feed - article"
-    link = "feeds/posts/"
-    description = "RSS feed - blog posts"
+    # title = "RSS feed - article"
+    # link = "feeds/posts/"
+    # description = "RSS feed - blog posts"
+    #
+    # def items(self):
+    #     return Article.objects.order_by('-date_time')
+    #
+    # def item_title(self, item):
+    #     return item.title
+    #
+    # def item_pubdate(self, item):
+    #     return item.date_time
+    #
+    # def item_description(self, item):
+    #     return item.content
 
+    # 订阅标题
+    title = u"许刚的博客的更新"
+    link = "/feeds/posts/"
+    # 描述
+    description = "关注小蜗牛的最新动态"
+
+    # 订阅的数据
     def items(self):
         return Article.objects.order_by('-date_time')
+        # 订阅的标题
 
     def item_title(self, item):
         return item.title
-
-    def item_pubdate(self, item):
-        return item.date_time
+        # 订阅的表示
 
     def item_description(self, item):
-        return item.content
+        return item.abstract
+        # 每条订阅的链接
+
+    def item_link(self, item):
+        return "http://www.along.party:8080/" + str(item.id) + "/"
